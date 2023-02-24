381afdd2713674d7cb1c6648d845335b4e6f9a6f
forooz <forooz.iraji@gmail.com>
2023-02-24 18:53:56 +0330

    Initial commit

---
+++ lexical_analysis/compiler.py
@@ -0,0 +1,6 @@
+# Alireza Foroodniya 99105645, Foroozan Iraji 99105272
+
+
+def get_next_token():
+    print("start")
+    # return (token type,token string)
---
+++ main.py
@@ -0,0 +1,6 @@
+from lexical_analysis.compiler import get_next_token
+
+
+def main():
+    while 1:
+        get_next_token()
