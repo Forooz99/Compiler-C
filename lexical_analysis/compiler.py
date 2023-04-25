b1ffbd8066b9867e07e844f749a554a79452a18e
forooz <forooz.iraji@gmail.com>
2023-04-25 22:29:42 +0330

    add grammar

--- lexical_analysis/compiler.py
+++ lexical_analysis/compiler.py
@@ -9,6 +9,54 @@ token_list = []
 error_list = []
 characterBuffer = None
 lineno = 1
+input_file = None
+lookahead = None
+grammar = {
+    "Program": [["Declaration-list"]],
+    "Declaration-list": [["Declaration", "Declaration-list"], ["EPSILON"]],
+    "Declaration": [["Declaration-initial", "Declaration-prime"]],
+    "Declaration-initial": [["Type-specifier", "ID"]],
+    "Declaration-prime": [["Fun-declaration-prime"], ["Var-declaration-prime"]],
+    "Var-declaration-prime": [[";"], ["[", "NUM", "]", ";"]],
+    "Fun-declaration-prime": [["(", "Params", ")", "Compound-stmt"]],
+    "Type-specifier": [["int"], ["void"]],
+    "Params": [["int", "ID", "Param-prime", "Param-list"], ["void"]],
+    "Param-list": [[",", "Param", "Param-list"], ["EPSILON"]],
+    "Param": [["Declaration-initial", "Param-prime"]],
+    "Param-prime": [["[", "]"], ["EPSILON"]],
+    "Compound-stmt": [["{", "Declaration-list", "Statement-list", "}"]],
+    "Statement-list": [["Statement", "Statement-list"], ["EPSILON"]],
+    "Statement": [["Expression-stmt"], ["Compound-stmt"], ["Selection-stmt"], ["Iteration-stmt"], ["Return-stmt"]],
+    "Expression-stmt": [["Expression", ";"], ["break", ";"], [";"]],
+    "Selection-stmt": [["if", "(", "Expression", ")", "Statement", "else", "Statement"]],
+    "Iteration-stmt": [["repeat", "Statement", "until", "(", "Expression", ")"]],
+    "Return-stmt": [["return", "Return-stmt-prime"]],
+    "Return-stmt-prime": [["Expression", ";"], [";"]],
+    "Expression": [["Simple-expression-zegond"], ["ID", "B"]],
+    "B": [["=", "Expression"], ["[", "Expression", "]", "H"], ["Simple-expression-prime"]],
+    "H": [["=", "Expression"], ["G", "D", "C"]],
+    "Simple-expression-zegond": [["Additive-expression-zegond", "C"]],
+    "Simple-expression-prime": [["Additive-expression-prime", "C"]],
+    "C": [["Relop", "Additive-expression"], ["EPSILON"]],
+    "Relop": [["<"], ["=="]],
+    "Additive-expression": [["Term", "D"]],
+    "Additive-expression-prime": [["Term-prime", "D"]],
+    "Additive-expression-zegond": [["Term-zegond", "D"]],
+    "D": [["Addop", "Term", "D"], ["EPSILON"]],
+    "Addop": [["+"], ["-"]],
+    "Term": [["Factor", "G"]],
+    "Term-prime": [["Factor-prime", "G"]],
+    "Term-zegond": [["Factor-zegond", "G"]],
+    "G": [["*", "Factor", "G"], ["EPSILON"]],
+    "Factor": [["(", "Expression", ")"], ["ID", "Var-call-prime"], ["NUM"]],
+    "Var-call-prime": [["(", "Args", ")"], ["Var-prime"]],
+    "Var-prime": [["[", "Expression", "]"], ["EPSILON"]],
+    "Factor-prime": [["(", "Args", ")"], ["EPSILON"]],
+    "Factor-zegond": [["(", "Expression", ")"], ["NUM"]],
+    "Args": [["Arg-list"], ["EPSILON"]],
+    "Arg-list": [["Expression", "Arg-list-prime"]],
+    "Arg-list-prime": [[",", "Expression", "Arg-list-prime"], ["EPSILON"]]
+}
 
 
 class Type(Enum):
@@ -53,11 +101,13 @@ class Error:
 
 
 def main():
+    global lookahead, input_file
     # initialize keywords as first tokens & add to symbol_table
     for keyword in keywords:
         Token(keyword, Type.KEYWORD, needToAddToTokenList=False)
 
     input_file = open("input.txt", "r")
+    lookahead = get_next_token(input_file)
 
     while get_next_token(input_file):
         get_next_token(input_file)
@@ -299,5 +349,12 @@ def findType(token):
     else:
         return Type.ID
 
+
+def match(terminal):
+    global lookahead, input_file
+    if lookahead == terminal:
+        lookahead = get_next_token(input_file)
+    else:
+        return "error"
 
 main()
